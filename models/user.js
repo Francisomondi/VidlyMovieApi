31bfefd0d169d1a52b7c3f9b65f69d32ad43f1d3
francis omondi <francisomondi17@gmail.com>
2022-09-21 11:33:28 +0300

    route to register new user

--- models/user.js
+++ models/user.js
@@ -16,6 +16,21 @@ const User = mongoose.model('User', new mongoose.Schema({
         unique: true
     },
     password: {
-        type: Password
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
     }
 }));
+
+function validateUser(user) {
+    const schema = {
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    }
+    return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
