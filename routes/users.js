4cc388b9f7ff9d8a95d3d0e86785a9ef4f3bdf5f
francis omondi <francisomondi17@gmail.com>
2022-09-21 16:46:31 +0300

    hashing passwords and using lodash

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,9 @@ const {
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+const _ = require("lodash")
+const bcrypt = require("bcrypt")
+
 
 router.post('/', async (req, res) => {
     const {
@@ -15,16 +18,16 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({
         email: req.body.email
     })
-    if (user) return res.status(400).send("Email is already registerd")
+    if (user) return res.status(400).send("User is already registerd")
 
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
+    user = new User(_.pick(req.body, ["name", "email", "password"]))
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(user.password, salt)
 
     await user.save()
-    res.send(user)
+
+
+    res.send(_.pick(user, ["_id", "name", "email"]))
 });
 
 module.exports = router
